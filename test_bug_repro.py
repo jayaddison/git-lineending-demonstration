34c2c1eb706ccb33119c8193a8c97264798cc9d3
James Addison <jay@jp-hosting.net>
2024-04-20 17:01:00 +0100

    Narrow the test case: the cause appears to be platorm-specific git reformatting of line-endings during code checkout.

--- test_bug_repro.py
+++ test_bug_repro.py
@@ -1,20 +1,12 @@
 from unittest import TestCase
 
 from hashlib import sha512
-from os import makedirs
 from pathlib import Path
-from shutil import copyfile
 
 
-class ShutilCopier(TestCase):
+class SHA512Test(TestCase):
     def test_bug_repro(self):
-        infile = Path("origin") / "file.txt"
-        infile_digest = sha512(infile.read_bytes()).hexdigest()
+        file = Path("origin") / "file.txt"
+        digest = sha512(file.read_bytes()).hexdigest()
 
-        makedirs("destination", exist_ok=True)
-        copyfile("origin/file.txt", "destination/file.txt")
-
-        outfile = Path("destination") / "file.txt"
-        outfile_digest = sha512(outfile.read_bytes()).hexdigest()
-
-        self.assertEqual(outfile_digest, infile_digest)
+        self.assertEqual("8d47d372496b4eb8a324a1e481a195c6506757dfabefe1b032a945a1f2c28f7797440bf9252614c6b533774b6dad304df81df4379cffc32a58d8bd58e8ed91e6", digest)
